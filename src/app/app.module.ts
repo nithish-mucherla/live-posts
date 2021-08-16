3b9dda14f6590f1ffa424e2213c5137ba8fefddd
nithish-mucherla <mucherlanithu@gmail.com>
2021-08-16 14:32:39 +0530

    add router and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { AuthComponent } from './auth/auth.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
+const routes: Routes = [
+  {
+    path: 'post-list',
+    component: PostListComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
+    path: 'post-edit',
+    component: PostEditComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'post-list',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostComponent,
+    AuthComponent,
+    PostEditComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes), ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
